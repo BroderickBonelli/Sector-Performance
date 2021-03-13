d8ece76f4cf13f3e06fe38a58eb5474295849e26
BroderickBonelli <79765357+BroderickBonelli@users.noreply.github.com>
2021-03-13 15:08:54 -0800

    Update SectorPerformance.py
    Plot sector performance on a downloadable bar chart, w/ branded colors

--- SectorPerformance.py
+++ SectorPerformance.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 sectors = ['XLC', 'XLY', 'XLP', 'XLE', 'XLF', 'XLV', 'XLI', 'XLB', 'XLRE', 'XLU', 'SPY']
 
@@ -11,14 +13,31 @@ for each_sector in sectors:
 	r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 	soup = BeautifulSoup(r.text, 'lxml')
 
-	perf_week = soup.find('td', string='Perf Week').find_next('td').text
-	perf_month = soup.find('td', string='Perf Month').find_next('td').text
-	perf_quarter = soup.find('td', string='Perf Quarter').find_next('td').text
-	perf_half_year = soup.find('td', string='Perf Half Y').find_next('td').text
-	perf_year = soup.find('td', string='Perf Year').find_next('td').text
+	perf_week = soup.find('td', string='Perf Week').find_next('td').text.strip('%')
+	perf_month = soup.find('td', string='Perf Month').find_next('td').text.strip('%')
+	perf_quarter = soup.find('td', string='Perf Quarter').find_next('td').text.strip('%')
+	perf_half_year = soup.find('td', string='Perf Half Y').find_next('td').text.strip('%')
+	perf_year = soup.find('td', string='Perf Year').find_next('td').text.strip('%')
 
-	df_list.append([each_sector, perf_week, perf_month, perf_quarter, perf_half_year, perf_year])
+	df_list.append([each_sector, float(perf_week), float(perf_month), float(perf_quarter), float(perf_half_year), float(perf_year)])
 
-df_columns = ['Ticker', '1w', '1mo', '3mo', '6mo', '1y']
+df_columns = ['Sector', '1w', '1mo', '3mo', '6mo', '1y']
 df = pd.DataFrame(df_list, columns=df_columns)
-print(df)
+#print(df)
+
+sns.set_palette('pastel')
+
+sns.barplot(x=df['Sector'], y=df['1w'], color='#3f616f')
+plt.show()
+
+sns.barplot(x=df['Sector'], y=df['1mo'], color='#3f616f')
+plt.show()
+
+sns.barplot(x=df['Sector'], y=df['3mo'], color='#3f616f')
+plt.show()
+
+sns.barplot(x=df['Sector'], y=df['6mo'], color='#3f616f')
+plt.show()
+
+sns.barplot(x=df['Sector'], y=df['1y'], color='#3f616f')
+plt.show()
